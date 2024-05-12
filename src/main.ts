f0943d7a709ac799fb3b233215cfc98635963042
“SEU-ShangyuanLi“ <li2488317771@gmail.com>
2024-05-12 17:28:39 +0800

    5.12commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue'//
 import App from './App.vue'
+import ElementPlus from 'element-plus'//导入
+import 'element-plus/dist/index.css'//导入样式
+import router from './router';
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
+App.use(ElementPlus)
