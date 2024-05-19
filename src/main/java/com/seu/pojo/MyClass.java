b911ef92656d41c387ce1469146ef8dd2ce582d9
lappland <2467528584@qq.com>
2024-05-19 16:43:04 +0800

    排课功能实现, SpringBoot版本升级, JWT令牌解析优化

--- src/main/java/com/seu/pojo/MyClass.java
+++ src/main/java/com/seu/pojo/MyClass.java
@@ -2,11 +2,8 @@ package com.seu.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-<<<<<<< HEAD:src/main/java/com/seu/pojo/ApplyingCourse.java
 import lombok.EqualsAndHashCode;
-=======
-import lombok.NoArgsConstructor;
->>>>>>> deaf9a05ae932250df0c112ec1261c939249a434:src/main/java/com/seu/pojo/MyClass.java
+
 
 @Data
 //@NoArgsConstructor
