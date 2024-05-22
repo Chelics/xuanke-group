e0664f163f646b4d3d3d66622f472eb20babd837
“SEU-ShangyuanLi“ <li2488317771@gmail.com>
2024-05-22 22:09:58 +0800

    5.22课表完成

--- VueAPP/src/views/Teacher/TeacherSchedule.vue
+++ VueAPP/src/views/Teacher/TeacherSchedule.vue
@@ -1,3 +1,7 @@
 <template>
     <h1>TeacherSchedule</h1>
+    <htmlTable url="/teacher/courseTable"></htmlTable>
 </template>
+<script setup lang="ts">
+import htmlTable from '@/testview/htmlTable.vue';
+</script>
